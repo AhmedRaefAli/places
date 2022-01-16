ee65c900f40e25db28f227319be404e45f8991b0
Ahmed Raef <74055411+AhmedRaefAli@users.noreply.github.com>
2022-01-16 10:41:21 +0200

    message

--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -1,8 +1,9 @@
 import { createContext } from 'react';
-//createContext is used to create a context 
+
 export const AuthContext = createContext({
   isLoggedIn: false,
   userId: null,
+  token: null,
   login: () => {},
   logout: () => {}
 });
